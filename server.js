500cd3e42ffb5a9afd43c4b2ba42bda48cc67e15
akulaakula1313-cpu <akulaakula1313@gmail.com>
2026-09-07 14:36:18 +0400

    Fix comment for unlockedMarkers in server.js
    Fixed comment regarding the unlockedMarkers initialization.

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ function defaultUser(name) {
     name: name || 'Премиум_Игрок_' + Math.floor(1000 + Math.random() * 9000),
     avatar: 0, 
     marker: 0, 
-    unlockedMarkers: [0],
+    unlockedMarkers: [0], // Ошибка исправлена здесь!
     bills: 5000, 
     coins: 50, 
     dailyStreak: 0, 
